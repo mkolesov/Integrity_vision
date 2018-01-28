fcedeec9fdcf9c3b92da22b748da5924df91907e
Kolesov <infernal27@yandex.ru>
2018-01-28 20:46:57 +0200

    tmp

--- src/main/java/app/FileReader.java
+++ src/main/java/app/FileReader.java
@@ -10,7 +10,8 @@ public class FileReader {
 
     private String rootAdress = "src/main/resources/";
     private ArrayList<String> wordList;
-    private HashMap<String, String> containsWord;;
+    private HashMap<String, Set<String>> containsWord;
+    private HashMap<String, Set<String>> containsWord2;
     private Set<String> concatenatedWords;
     private String longestWord = "";
     private String secondLongestWord = "";
@@ -18,7 +19,8 @@ public class FileReader {
     public void process(String fileName){
         wordList = readFromFile(rootAdress+fileName);
         Iterator<String> iterator = wordList.iterator();
-        containsWord = new HashMap<String, String>();
+        containsWord = new HashMap<String, Set<String>>();
+        containsWord2 = new HashMap<String, Set<String>>();
         concatenatedWords = new HashSet<String>();
         System.out.println("Starting processing....");
         int i = 0;
@@ -34,6 +36,7 @@ public class FileReader {
                 i=0;
             }
         }
+        getRes();
         System.out.println("Finished.");
     }
 
@@ -54,22 +57,94 @@ public class FileReader {
             String checkWord = secondIterator.next();
             //Если проверяемое слово содержит заданное слово и не является им
             if (checkWord.contains(word) & !checkWord.equals(word)){
-                //Если в списке кандидатов уже есть это слово
-                if(containsWord.containsKey(checkWord)){
-                    String createrWord = containsWord.get(checkWord);
-                    //Проверка является заданное слово сокоренным кандидату или составным
-                    if (!word.contains(createrWord)& !createrWord.contains(word)){
-                        if (!concatenatedWords.contains(checkWord)){
-                            checkLenght(checkWord);
-                            concatenatedWords.add(checkWord);
-
-                        }
-                    }
+                //
+                if(!containsWord.containsKey(checkWord)){
+                    HashSet <String> newSet = new HashSet<String>();
+                    newSet.add(word);
+                    containsWord.put(checkWord, newSet);
                 } else {
-                    containsWord.put(checkWord, word);
+                    containsWord.get(checkWord).add(word);
                 }
+
+
+                //Если в списке кандидатов уже есть это слово
+//                if(containsWord.containsKey(checkWord)){
+//                    String createrWord = containsWord.get(checkWord);
+//                    //Проверка является заданное слово сокоренным кандидату или составным
+//                    if (!word.contains(createrWord)& !createrWord.contains(word)){
+//                        if (!concatenatedWords.contains(checkWord)){
+//                            checkLenght(checkWord);
+//                            concatenatedWords.add(checkWord);
+//
+//                        }
+//                    }
+//                } else {
+//                    containsWord.put(checkWord, word);
+//                }
             }
         }
+    }
+
+    private void getRes(){
+        Iterator<String> mapIterator = containsWord.keySet().iterator();
+        while (mapIterator.hasNext()) {
+            String key = mapIterator.next();
+//            if (key.equals("catallycatscatses")){
+//                System.out.println("GOT YOu");
+//            };
+            Set<String> set = containsWord.get(key);
+            List<String> TESTset = new ArrayList<String>();
+            Iterator<String> setIterator1 = set.iterator();
+            while ( setIterator1.hasNext()) {
+                String word1 = setIterator1.next();
+                Iterator<String> setIterator2 = set.iterator();
+                boolean isInherit = false;
+                while (setIterator2.hasNext()) {
+                    String word2 = setIterator2.next();
+                    if (word2.contains(word1) & !word2.equals(word1) &
+                            key.indexOf(word1) == key.indexOf(word2)) {
+                        isInherit = true;
+                        break;
+                    }
+                }
+                if (!isInherit){
+                    TESTset.add(word1);
+                }
+            }
+            Comparator<String> my = new MyComparator();
+            Collections.sort(TESTset, my);
+            String tmp = key;
+            for (String s:TESTset){
+                tmp = tmp.replace(s, "");
+            }
+            if(tmp.length()==0){
+                concatenatedWords.add(key);
+                checkLenght(key);
+            }
+        }
+
+//            while ( setIterator1.hasNext()){
+//                String word1 = setIterator1.next();
+//                Iterator<String> setIterator2 = set.iterator();
+//                boolean isInherit = false;
+//                while ( setIterator2.hasNext()){
+//                    String word2 = setIterator2.next();
+//                    if (word2.contains(word1) & !word2.equals(word1)&
+//                            key.indexOf(word1)==key.indexOf(word2)){
+//                        isInherit = true;
+//                        break;
+//                    }
+//                }
+//                if (!isInherit){
+//                    i++;
+//                }
+//                if (i>1){
+//                    concatenatedWords.add(key);
+//                    containsWord2.put(key, set);
+//                }
+//            }
+//            System.out.println("test");
+//        }
     }
 
     private ArrayList<String> readFromFile(String fileAdress){

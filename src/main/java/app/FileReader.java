0de63731dbe0d643cb2bcdfc4d3eb965267956a7
Kolesov <infernal27@yandex.ru>
2018-01-29 02:38:59 +0200

    REALESE 2.0

--- src/main/java/app/FileReader.java
+++ src/main/java/app/FileReader.java
@@ -10,24 +10,28 @@ public class FileReader {
 
     private String rootAdress = "src/main/resources/";
     private ArrayList<String> wordList;
-    private HashMap<String, Set<String>> containsWord;
+    private HashMap<String, Set<String>> containsWordInside;
     private Set<String> concatenatedWords;
     private String longestWord = "";
     private String secondLongestWord = "";
-    private Set<String> set;
+    private Set<String> currentWordInnerSet;
 
     public void process(String fileName){
         wordList = readFromFile(rootAdress+fileName);
+        processWordsList();
+    }
+
+    private void processWordsList(){
+        System.out.println("Processing:");
+        System.out.println("    -Finding words usages:");
         Iterator<String> iterator = wordList.iterator();
-        containsWord = new HashMap<String, Set<String>>();
-        concatenatedWords = new HashSet<String>();
-        System.out.println("Starting processing....");
+        containsWordInside = new HashMap<String, Set<String>>();
         int i = 0;
         int currentPercent = 0;
         int percent = wordList.size()/100;
-        System.out.print("Current progress: "+currentPercent+"% ");
+        System.out.print("      Current progress: "+currentPercent+"% ");
         while (iterator.hasNext()){
-            processWord(iterator.next());
+            findWordUsage(iterator.next());
             i++;
             if (i == percent){
                 currentPercent++;
@@ -35,7 +39,9 @@ public class FileReader {
                 i=0;
             }
         }
-        getRes();
+        System.out.println("Finished.");
+        System.out.print("  -Finding concateneted words.....");
+        testConcatenetion();
         System.out.println("Finished.");
     }
 
@@ -50,188 +56,76 @@ public class FileReader {
         }
     }
 
-    private void processWord(String word){
+    private void findWordUsage(String word){
+        /*
+        * Тут можно было бы ускорить поиск
+        * отсортировав по длине слов весь список от малых к большим
+        * а затем искать только в возрастающих по длине
+        * но уже так оставлю, со старта не догадался ;)
+        * */
         Iterator<String> secondIterator = wordList.iterator();
         while (secondIterator.hasNext()){
             String checkWord = secondIterator.next();
             //Если проверяемое слово содержит заданное слово и не является им
             if (checkWord.contains(word) & !checkWord.equals(word)){
-                //
-                if(!containsWord.containsKey(checkWord)){
+                //Если слова нет в списке кандидатов - добавляем его и список слов в нем
+                if(!containsWordInside.containsKey(checkWord)){
                     HashSet <String> newSet = new HashSet<String>();
                     newSet.add(word);
-                    containsWord.put(checkWord, newSet);
-                } else {
-                    containsWord.get(checkWord).add(word);
+                    containsWordInside.put(checkWord, newSet);
                 }
-
-
-                //Если в списке кандидатов уже есть это слово
-//                if(containsWord.containsKey(checkWord)){
-//                    String createrWord = containsWord.get(checkWord);
-//                    //Проверка является заданное слово сокоренным кандидату или составным
-//                    if (!word.contains(createrWord)& !createrWord.contains(word)){
-//                        if (!concatenatedWords.contains(checkWord)){
-//                            checkLenght(checkWord);
-//                            concatenatedWords.add(checkWord);
-//
-//                        }
-//                    }
-//                } else {
-//                    containsWord.put(checkWord, word);
-//                }
+                //Если есть, то пополняем список
+                else {
+                    containsWordInside.get(checkWord).add(word);
+                }
             }
         }
     }
 
-    private boolean rec(String word, String concat, int index){
-
-        Iterator<String> setIterator1 = set.iterator();
-
-        while ( setIterator1.hasNext()) {
-            String s = setIterator1.next();
-            if (word.indexOf(s, index) == index) {
-                concat = concat.concat(s);
-                if (word.equals(concat)) {
+    private boolean recursive(String word, String concatenatedWord, int index){
+        Iterator<String> setIterator = currentWordInnerSet.iterator();
+        while ( setIterator.hasNext()) {
+            String currentTestingWord = setIterator.next();
+            //Если текущее слово находится по заданному индексу
+            if (word.indexOf(currentTestingWord, index) == index) {
+                concatenatedWord = concatenatedWord.concat(currentTestingWord);
+                if (word.equals(concatenatedWord)) {
                     concatenatedWords.add(word);
                     checkLenght(word);
                     return true;
                 }
-                if (rec(word, concat, index + s.length())) {
+                if (recursive(word, concatenatedWord, index + currentTestingWord.length())) {
                     return true;
                 } else {
-                    concat = concat.substring(0, index);
+                    concatenatedWord = concatenatedWord.substring(0, index);
                 }
             }
-
-//            if (word.indexOf(s, index)<0){
-//                continue;
-//            }
         }
         return false;
     }
 
-    private void getRes(){
-        Iterator<String> mapIterator = containsWord.keySet().iterator();
+    private void testConcatenetion(){
+        concatenatedWords = new HashSet<String>();
+        Iterator<String> mapIterator = containsWordInside.keySet().iterator();
         while (mapIterator.hasNext()) {
             String key = mapIterator.next();
-            set = containsWord.get(key);
-            rec(key, "", 0);
-//            Set<String> set = containsWord.get(key);
-//            Iterator<String> setIterator1 = set.iterator();
-//
-//            //new 2
-//            while ( setIterator1.hasNext()) {
-//                String s = setIterator1.next();
-
-
-            //new stuff
-            //if (key.equals("catsdogcat")){
-//            if (key.equals("electroencephalographically")){
-//                System.out.println("GOT YOu");
-//            };
-//            int [] mass = new int[key.length()];
-//            Arrays.fill(mass, 0);
-//            while ( setIterator1.hasNext()) {
-//                String s = setIterator1.next();
-//
-//
-//                int index = key.indexOf(s);
-//                while (index>-1){
-//                    Arrays.fill(mass, index, index+s.length(), 1);
-//                    index = key.indexOf(s, index+s.length());
-//                }
-//
-//
-//            }
-//
-//            Arrays.sort(mass);
-//            if (Arrays.binarySearch(mass, 0)<0){
-//                concatenatedWords.add(key);
-//                checkLenght(key);
-//            }
-
-            //old
-//            List<String> TESTset = new ArrayList<String>();
-//            while ( setIterator1.hasNext()) {
-//                String word1 = setIterator1.next();
-//                Iterator<String> setIterator2 = set.iterator();
-//                boolean isInherit = false;
-//                while (setIterator2.hasNext()) {
-//                    String word2 = setIterator2.next();
-//                    if (word2.contains(word1) & !word2.equals(word1) &
-//                            key.indexOf(word1) == key.indexOf(word2)) {
-//                        isInherit = true;
-//                        break;
-//                    }
-//                }
-//                if (!isInherit){
-//                    TESTset.add(word1);
-//                }
-//            }
-//            Comparator<String> my = new MyComparator();
-//            Collections.sort(TESTset, my);
-//            String tmp = key;
-//            for (String s:TESTset){
-//                tmp = tmp.replace(s, "");
-//            }
-//            if(tmp.length()==0){
-//                concatenatedWords.add(key);
-//                checkLenght(key);
-//            }
-
-
-
-
-
-
-//             if (key.equals("ethylenediaminetetraacetates")){
-//                System.out.println("GOT YOu");
-//            };
-//            if(TESTset.size()>1){
-//                concatenatedWords.add(key);
-//                checkLenght(key);
-//            }
+            currentWordInnerSet = containsWordInside.get(key);
+            recursive(key, "", 0);
         }
-
-//            while ( setIterator1.hasNext()){
-//                String word1 = setIterator1.next();
-//                Iterator<String> setIterator2 = set.iterator();
-//                boolean isInherit = false;
-//                while ( setIterator2.hasNext()){
-//                    String word2 = setIterator2.next();
-//                    if (word2.contains(word1) & !word2.equals(word1)&
-//                            key.indexOf(word1)==key.indexOf(word2)){
-//                        isInherit = true;
-//                        break;
-//                    }
-//                }
-//                if (!isInherit){
-//                    i++;
-//                }
-//                if (i>1){
-//                    concatenatedWords.add(key);
-//                    containsWord2.put(key, set);
-//                }
-//            }
-//            System.out.println("test");
-//        }
     }
 
     private ArrayList<String> readFromFile(String fileAdress){
         ArrayList<String> result = new ArrayList<String>();
-        System.out.println("Reading file....");
+        System.out.print("Reading file.......");
         try {
             RandomAccessFile randomAccessFile = new RandomAccessFile(fileAdress, "r");
             String word = randomAccessFile.readLine();
             while (word != null && word.length()>0){
                 result.add(word);
                 word = randomAccessFile.readLine();
-//                if(word.length()==27){
-//                    System.out.println(27);
-//                }
             }
             randomAccessFile.close();
+            System.out.println("Finished.");
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
--- src/main/java/app/myApp.java
+++ src/main/java/app/myApp.java
@@ -8,9 +8,8 @@ public class myApp {
     public static void main(String[] args) {
         FileReader fileReader = new FileReader();
         fileReader.process("words.txt");
-        //fileReader.process("test.txt");
-        System.out.println("Longest concateneted word: "+fileReader.getLongestWord());
-        System.out.println("Second longest concateneted word: "+fileReader.getSecondLongestWord());
+        System.out.println("Longest concateneted word ("+fileReader.getLongestWord().length()+" chars): "+fileReader.getLongestWord());
+        System.out.println("Second longest concateneted word ("+fileReader.getSecondLongestWord().length()+" chars): "+fileReader.getSecondLongestWord());
         System.out.println("Count of concateneted words: "+fileReader.getConcatenetedWordsCount());
     }
 
